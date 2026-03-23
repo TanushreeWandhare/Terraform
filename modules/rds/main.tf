77079b9aa120782bbb5ed90e3659d8731ade6e3c
TanushreeWandhare <tanushree.wandhare03@gmail.com>
2026-03-23 23:21:44 +0530

    fix

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -59,7 +59,7 @@ resource "aws_db_instance" "cbz_db_instance" {
 
 # Create a DB Subnet Group using default subnets
 resource "aws_db_subnet_group" "default" {
-  name       = "default-db-subnet-group-${var.project}"
+  name       = "${var.project}-${var.env}-db-subnet-group"
   subnet_ids = data.aws_subnets.default.ids
   tags = {
     Name        = "default-db-subnet-group"
